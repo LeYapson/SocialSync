da90e215369eb974b215d8b022f41d4139475d92
théau Yapi <theau.yapi@ynov.com>
2025-05-12 11:16:54 +0200

    on reprend de zero

--- SocialSync/src/app/app.component.spec.ts
+++ SocialSync/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SocialSync' title`, () => {
+  it(`should have the 'socialsync' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SocialSync');
+    expect(app.title).toEqual('socialsync');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SocialSync');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, socialsync');
   });
 });
