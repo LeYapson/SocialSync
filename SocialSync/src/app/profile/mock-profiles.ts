350dcceb23fff4cbb453102ce919bcf3e303f524
théau Yapi <theau.yapi@ynov.com>
2025-05-12 09:46:35 +0200

    debut projet

---
+++ SocialSync/src/app/profile/mock-profiles.ts
@@ -0,0 +1,18 @@
+import { Profile } from "../interface/profile";
+
+export const PROFILES: Profile[] = [
+  {
+    "id": 1,
+    "firstName": "Terry",
+    "lastName": "Medhurst",
+    "maidenName": "Smitham",
+    "age": 50,
+    "gender": "male",
+    "email": "atuny0@sohu.com",
+    "phone": "+63 791 675 8914",
+    "username": "atuny0",
+    "password": "9uQFF1Lh",
+    "birthDate": "2000-12-25",
+    "image": "https://robohash.org/Terry.png?set=set4"
+  }
+];
